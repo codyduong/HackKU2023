6c29340fa21033f99fa786ddd822278c00d70d26
codyduong <duongcody507@gmail.com>
2023-04-15 07:42:26 -0500

    Add queries and viewing mode

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -3,18 +3,77 @@ import { Loader } from '@googlemaps/js-api-loader';
 import { isServer } from 'solid-js/web';
 import { useUser } from '~/context/User';
 import Button from '~/components/Button';
-import { createSignal, Show, For } from 'solid-js';
-import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { createSignal, Show, For, createEffect } from 'solid-js';
+import {
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from 'firebase/firestore';
 import { db } from '~/auth';
 import { ImCancelCircle } from 'solid-icons/im';
 
-let map: google.maps.Map;
+type SelectPlaceDetails = Pick<
+  google.maps.places.PlaceResult,
+  'icon' | 'icon_background_color'
+>;
+
+export type Place = {
+  placeId: null | string;
+  latLng: null | google.maps.LatLng;
+} & SelectPlaceDetails;
+
+export type PlaceFirestore = {
+  lat: null | number;
+  lng: null | number;
+  tags: null | string[];
+} & SelectPlaceDetails;
+
+const styles: Record<string, google.maps.MapTypeStyle[]> = {
+  default: [],
+  hide: [
+    {
+      featureType: 'poi',
+      elementType: 'labels.icon',
+      stylers: [{ visibility: 'off' }],
+    },
+    {
+      featureType: 'transit',
+      elementType: 'labels.icon',
+      stylers: [{ visibility: 'off' }],
+    },
+  ],
+};
 
 export default function Dashboard() {
   const [user, { signOut }] = useUser();
   const [tag, setTag] = createSignal('');
   const [tags, setTags] = createSignal<string[]>([]);
-  const [placeId, setPlaceId] = createSignal('');
+  const [place, setPlace] = createSignal<Place>({
+    placeId: null,
+    latLng: null,
+  });
+  const [mapBounds, setMapBounds] = createSignal<{
+    lat: {
+      lower: number;
+      upper: number;
+    };
+    lng: {
+      lower: number;
+      upper: number;
+    };
+  }>();
+  const [map, setMap] = createSignal<google.maps.Map>();
+  const [mapChanged, setMapChanged] = createSignal(false);
+  const [loading, setLoading] = createSignal(false);
+  const [markers, setMarkers] = createSignal<google.maps.Marker[]>([]);
+  // Either we are 'Creating' or 'Viewing'
+  const [viewingMode, setViewingMode] = createSignal<'creating' | 'viewing'>(
+    'creating'
+  );
 
   if (!isServer) {
     const loader = new Loader({
@@ -30,42 +89,154 @@ export default function Dashboard() {
       const { PlacesService } = (await google.maps.importLibrary(
         'places'
       )) as google.maps.PlacesLibrary;
-      map = new Map(document.getElementById('map') as HTMLElement, {
+      const m = new Map(document.getElementById('map') as HTMLElement, {
         center: { lat: 38.9543, lng: -95.2558 },
         zoom: 16,
       });
-      map.addListener(
+      const p = new PlacesService(m);
+      m.addListener(
         'click',
         async (e: google.maps.MapMouseEvent | google.maps.IconMouseEvent) => {
           if ('placeId' in e && e.placeId) {
+            // get other info from placeId
+            const details =
+              await new Promise<google.maps.places.PlaceResult | null>(
+                (resolve) => {
+                  p.getDetails({ placeId: e.placeId! }, (a) => resolve(a));
+                }
+              );
+
+            // add tags from firestore
             await searchPlace(e.placeId);
-            await setPlaceId(e.placeId);
+            setPlace({
+              placeId: e.placeId,
+              latLng: e.latLng,
+              icon: details?.icon,
+              icon_background_color: details?.icon_background_color,
+            });
           }
         }
       );
+      m.addListener('bounds_changed', async () => {
+        const southwest = m.getBounds()?.getSouthWest();
+        const northeast = m.getBounds()?.getNorthEast();
+        if (southwest && northeast) {
+          setMapBounds({
+            lat: {
+              lower: southwest.lat(),
+              upper: northeast.lat(),
+            },
+            lng: { lower: southwest.lng(), upper: northeast.lng() },
+          });
+          setMapChanged(true);
+        }
+      });
+      // eslint-disable-next-line solid/reactivity
+      m.addListener('idle', async () => {
+        const bounds = mapBounds();
+        if (mapChanged() && !loading() && bounds) {
+          console.log(mapChanged());
+          setLoading(true);
+          const locations = collection(db, 'locations');
+          const matchingLat = await getDocs(
+            query(
+              locations,
+              where('lat', '>=', bounds.lat.lower),
+              where('lat', '<=', bounds.lat.upper)
+            )
+          );
+          const matchingLng = await getDocs(
+            query(
+              locations,
+              where('lng', '>=', bounds.lng.lower),
+              where('lng', '<=', bounds.lng.upper)
+            )
+          );
+          const associativeArrayLat = matchingLat.docs.reduce(
+            (previousValue, currentValue) => {
+              const data = currentValue.data();
+              const obj = previousValue;
+              if (data) {
+                obj[currentValue.id] = data;
+              }
+              return obj;
+            },
+            {} as Record<string, unknown>
+          );
+          const associativeArrayLng = matchingLng.docs.reduce(
+            (previousValue, currentValue) => {
+              const data = currentValue.data();
+              const obj = previousValue;
+              if (data) {
+                obj[currentValue.id] = data;
+              }
+              return obj;
+            },
+            {} as Record<string, unknown>
+          );
+          const values: [placeId: string, place: PlaceFirestore][] = [];
+          for (const [key, value] of Object.entries(associativeArrayLat)) {
+            if (associativeArrayLng[key]) {
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              values.push([key, value as any]);
+            }
+          }
+          // unload old markers
+          // todo unload only no longer visible
+          markers().forEach((marker) => {
+            marker.setMap(null);
+          });
 
-      const styles: Record<string, google.maps.MapTypeStyle[]> = {
-        default: [],
-        hide: [
-          {
-            featureType: 'poi',
-            elementType: 'labels.icon',
-            stylers: [{ visibility: 'off' }],
-          },
-          {
-            featureType: 'transit',
-            elementType: 'labels.icon',
-            stylers: [{ visibility: 'off' }],
-          },
-        ],
-      };
-
-      // map.setOptions({ styles: styles.hide });
+          setMarkers(
+            values.map(([placeId, value]) => {
+              const latLngLiteral = { lat: value.lat!, lng: value.lng! };
+              const marker = new google.maps.Marker({
+                position: latLngLiteral,
+                icon: {
+                  url: value.icon!,
+                  size: new google.maps.Size(71, 71),
+                  origin: new google.maps.Point(0, 0),
+                  anchor: new google.maps.Point(17, 34),
+                  scaledSize: new google.maps.Size(25, 25),
+                },
+              });
+              marker.addListener('click', async () => {
+                // add tags from firestore
+                await searchPlace(placeId);
+                setPlace({
+                  placeId: placeId,
+                  latLng: new google.maps.LatLng(latLngLiteral),
+                  icon: value.icon,
+                  icon_background_color: value.icon_background_color,
+                });
+              });
+              return marker;
+            })
+          );
+          setLoading(false);
+          setMapChanged(false);
+        }
+      });
+      setMap(m);
     });
   }
 
+  createEffect(() => {
+    const m = map();
+    if (m) {
+      console.log(viewingMode());
+      if (viewingMode() === 'viewing') {
+        m.setOptions({ styles: styles.hide });
+        markers().forEach((marker) => marker.setMap(m));
+      } else {
+        m.setOptions({ styles: styles.default });
+        markers().forEach((marker) => marker.setMap(null));
+      }
+    }
+  });
+
   const clear = () => {
-    setPlaceId('');
+    setPlace({ placeId: null, latLng: null });
     setTag('');
     setTags([]);
   };
@@ -83,15 +254,24 @@ export default function Dashboard() {
   };
 
   const onSubmit = async () => {
-    if (!placeId()) {
+    const placeId = place().placeId;
+    const latLng = place().latLng;
+    if (!placeId) {
       throw new TypeError('No placeId specified!');
+    }
+    if (!latLng) {
+      throw new TypeError('No latLng specified!');
     }
     // if (tags().length === 0) {
     //   throw new TypeError('No tags specified!');
     // }
 
-    await setDoc(doc(db, 'locations', placeId()), {
+    await setDoc(doc(db, 'locations', placeId), {
+      lat: latLng.lat(),
+      lng: latLng.lng(),
       tags: tags(),
+      icon: place().icon,
+      icon_background_color: place().icon_background_color,
     });
 
     clear();
@@ -123,7 +303,6 @@ export default function Dashboard() {
           }
 
           .navbar {
-            position: absolute;
             flex-flow: row nowrap;
             width: 100vw;
             height: 4rem;
@@ -155,10 +334,10 @@ export default function Dashboard() {
         `}
       </style>
       <main>
-        <Show when={placeId()}>
+        <Show when={place().placeId}>
           <div class="modal">
             <section class="modal-section">
-              <h3>Editing place with placeId: ${placeId()}</h3>
+              <h3>Editing place with placeId: ${place().placeId}</h3>
               <input
                 class="input"
                 aria-label="Tag"
@@ -201,6 +380,16 @@ export default function Dashboard() {
         </Show>
         <section class="section navbar">
           <Button onClick={() => signOut()}>Sign Out</Button>
+          <Button
+            onClick={() => {
+              setViewingMode((p) =>
+                p === 'creating' ? 'viewing' : 'creating'
+              );
+            }}
+          >
+            Switch to {viewingMode() === 'creating' ? 'viewing' : 'creating'}{' '}
+            mode
+          </Button>
         </section>
         <div id="map" class="map" />
       </main>
