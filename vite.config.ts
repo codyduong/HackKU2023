278a08369550656ae1915640fe65c629074f41f1
codyduong <duongcody507@gmail.com>
2023-04-14 22:29:06 -0500

    Add font

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import solid from 'solid-start/vite';
 import { defineConfig } from 'vite';
 import solidStyled from 'vite-plugin-solid-styled';
+import Unfonts from 'unplugin-fonts/vite';
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,14 @@ export default defineConfig({
       filter: {
         include: 'src/**/*.tsx',
         exclude: 'node_modules/**/*.{ts,js}',
+      },
+    }),
+    Unfonts({
+      google: {
+        families: ['Atkinson Hyperlegible '],
+      },
+      fontsource: {
+        families: ['Atkinson Hyperlegible'],
       },
     }),
   ],
